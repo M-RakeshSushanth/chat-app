8e5791ad205352ec332fdb967759e8d3e82f9e98
M-RakeshSushanth <maremallarakesh07@gmail.com>
2026-07-14 22:58:43 +0530

    Add frontend setup

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignUp() {
+  return(
+    <div className="w-full h-[100vh] bg-slate-200 flex items-center justify-center" >
+      <div className="w-full w-max-[500px] h-[600px] bg-white rounded-lg shadow-gray-400 shadow-lg">
+
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
