468957eac25c9b886e1d856c9d1ff3bf6d95a2b0
M-RakeshSushanth <maremallarakesh07@gmail.com>
2026-07-16 22:48:07 +0530

    Added login page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {useNavigate} from "react-router-dom"
+import {serverUrl} from "../main.jsx"
+import axios from "axios"
+
 
 function SignUp() {
   let navigate = useNavigate()
+  let [show,setShow] = useState(false)
+  let [userName,setUserName] = useState("")
+  let [email,setEmail] = useState("")
+  let [password,setPassword] = useState("")
+  const handleSignUp = async()=>{
+    e.preventDefault()
+    try{
+        let result = await axios.post(`${serverUrl}/api/auth/signup`,{
+           userName,email,password
+        },{withCredentials:true})
+        console.log(result)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   return(
     <div className="w-full h-[100vh] bg-slate-200 flex items-center justify-center" >
       <div className="w-full max-w-[500px] h-[90vh] bg-white rounded-b-lg shadow-gray-400 shadow-lg flex flex-col gap-[30px]">
           <div className="w-full h-[40vh] max-h-[200px] bg-[#20c7ff] rounded-b-[30%] shadow-gray-400 shadow-lg flex items-center justify-center">
                <h1 className="text-gray-600 font-bold text-[29px]">welcome to <span className="text-white">chatly</span></h1>
           </div>
-          <form className="w-full flex flex-col gap-[15px] items-center">
-               <input type="text" placeholder="username" className="w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-white rounded-lg shadow-gray-200 shadow-lg"/>
-               <input type="email" placeholder="email" className="w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-white rounded-lg shadow-gray-200 shadow-lg"/>
-               <div className="w-[90%] h-[50px] border-2 border-[#20c7ff] overflow-hidden shadow-gray-200 shadow-lg rounded-lg">
-                 <input type="password" placeholder="password" className="w-full h-full outline-none px-[20px] py-[10px] bg-white"/>
-                 <span className="absolute top-0 right-0">show</span>
+          <form className="w-full flex flex-col gap-[15px] items-center" onSubmit={handleSignUp}>
+               <input type="text" placeholder="username" className="w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-white rounded-lg shadow-gray-200 shadow-lg" onChange={(e)=>{
+                  setUserName(e.target.value)
+               }}/>
+               <input type="email" placeholder="email" className="w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-white rounded-lg shadow-gray-200 shadow-lg" onChange={(e)=>{
+                  setEmail(e.target.value)
+               }}/>
+               <div className=" relative w-[90%] h-[50px] border-2 border-[#20c7ff] overflow-hidden shadow-gray-200 shadow-lg rounded-lg">
+                 <input type={show ? "text" : "password"} placeholder="password" className="w-full h-full outline-none px-[20px] py-[10px] bg-white" onChange={(e)=>{
+                  setPassword(e.target.value)
+                 }}/>
+                 <span className="absolute top-[50%] -translate-y-[50%] right-2 text-[#20c7ff] font-semibold cursor-pointer" onClick={()=>setShow(!show)}>{show?"Hide":"show"}</span>
                </div>
                <button className="px-[20px] py-[10px] bg-[#20c7ff] rounded-2xl shadow-gray-200 shadow-lg text-[20px] font-semibold hover:shadow-inner">Sign up</button>
 
